3f9c9ef5e62ee15e88c9c5c7995fea601a8aa114
William Squires <william.squiresii@gmail.com>
2023-04-07 14:43:14 -0500

    changed error handling to use custom error page

--- app.js
+++ app.js
@@ -27,9 +27,8 @@ app.use(function (req, res, next) {
 /** Error handler: logs stacktrace and returns JSON error message. */
 app.use(function (err, req, res, next) {
   const status = err.status || 500;
-  const message = err.message;
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
-  return res.status(status).json({ error: { message, status } });
+  res.render("error.html", { err });
 });
 
 module.exports = app;
